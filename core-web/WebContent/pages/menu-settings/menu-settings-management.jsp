9ec8887a7ebd464a003454efd65a4858b21ab452
Chen Xin Nien <chen.xin.nien@gmail.com>
2017-05-03 19:19:38 +0800

    update

---
+++ core-web/WebContent/pages/menu-settings/menu-settings-management.jsp
@@ -0,0 +1,48 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="q" uri="http://www.qifu.org/controller/tag" %>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>qifu</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<jsp:include page="../common-f-inc.jsp"></jsp:include>
+
+<style type="text/css">
+
+
+</style>
+
+
+<script type="text/javascript">
+
+</script>
+
+<body>
+
+<q:toolBar 
+	id="CORE_PROG001D0003Q_toolbar" 
+	refreshEnable="Y"
+	refreshJsMethod="window.location=parent.getProgUrl('CORE_PROG001D0003Q');" 
+	createNewEnable="N"
+	createNewJsMethod=""
+	saveEnabel="N" 
+	saveJsMethod="" 	
+	cancelEnable="Y" 
+	cancelJsMethod="parent.closeTab('CORE_PROG001D0003Q');" >
+</q:toolBar>
+<jsp:include page="../common-f-head.jsp"></jsp:include>
+
+xxxxxxxxxxxxxxxx
+
+</body>
+</html>
---
+++ core-web/src/org/qifu/controller/MenuSettingsAction.java
@@ -0,0 +1,60 @@
+/* 
+ * Copyright 2012-2017 qifu of copyright Chen Xin Nien
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * author: 	Chen Xin Nien
+ * contact: chen.xin.nien@gmail.com
+ * 
+ */
+package org.qifu.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.qifu.base.controller.BaseController;
+import org.qifu.base.exception.AuthorityException;
+import org.qifu.base.exception.ControllerException;
+import org.qifu.base.exception.ServiceException;
+import org.qifu.base.model.ControllerMethodAuthority;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Controller
+public class MenuSettingsAction extends BaseController {
+	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG001D0003Q")
+	@RequestMapping(value = "/core.menuSettingsManagement.do")	
+	public ModelAndView queryPage(HttpServletRequest request) {
+		String viewName = PAGE_SYS_ERROR;
+		ModelAndView mv = this.getDefaultModelAndView("CORE_PROG001D0003Q");
+		try {
+			// do some...
+			viewName = "menu-settings/menu-settings-management";
+		} catch (AuthorityException e) {
+			viewName = PAGE_SYS_NO_AUTH;
+		} catch (ServiceException | ControllerException e) {
+			viewName = PAGE_SYS_SEARCH_NO_DATA;
+		} catch (Exception e) {
+			e.printStackTrace();
+			this.setPageMessage(request, e.getMessage().toString());
+		}
+		mv.setViewName(viewName);
+		return mv;		
+	}
+
+}
