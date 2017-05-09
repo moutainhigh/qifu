a48a39c5401c3bf2b08108cdb0577e4fe04bb301
Chen Xin Nien <chen.xin.nien@gmail.com>
2017-05-09 09:27:09 +0800

    update

---
+++ core-web/src/org/qifu/controller/RoleAction.java
@@ -0,0 +1,109 @@
+/* 
+ * Copyright 2012-2017 qifu of copyright Chen Xin Nien
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * author: 	Chen Xin Nien
+ * contact: chen.xin.nien@gmail.com
+ * 
+ */
+package org.qifu.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.qifu.base.controller.BaseController;
+import org.qifu.base.exception.AuthorityException;
+import org.qifu.base.exception.ControllerException;
+import org.qifu.base.exception.ServiceException;
+import org.qifu.base.model.ControllerMethodAuthority;
+import org.qifu.base.model.PageOf;
+import org.qifu.base.model.QueryControllerJsonResultObj;
+import org.qifu.base.model.QueryResult;
+import org.qifu.base.model.SearchValue;
+import org.qifu.po.TbRole;
+import org.qifu.service.IRoleService;
+import org.qifu.vo.RoleVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Required;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Controller
+public class RoleAction extends BaseController {
+	
+	private IRoleService<RoleVO, TbRole, String> roleService;
+	
+	public IRoleService<RoleVO, TbRole, String> getRoleService() {
+		return roleService;
+	}
+
+	@Autowired
+	@Resource(name="core.service.RoleService")
+	@Required	
+	public void setRoleService(IRoleService<RoleVO, TbRole, String> roleService) {
+		this.roleService = roleService;
+	}
+
+	private void init(HttpServletRequest request, ModelAndView mv) throws ServiceException, ControllerException, Exception {
+		
+	}
+	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG002D0001Q")
+	@RequestMapping(value = "/core.roleManagement.do")	
+	public ModelAndView queryPage(HttpServletRequest request) {
+		String viewName = PAGE_SYS_ERROR;
+		ModelAndView mv = this.getDefaultModelAndView("CORE_PROG002D0001Q");
+		try {
+			this.init(request, mv);
+			viewName = "role/role-management";
+		} catch (AuthorityException e) {
+			viewName = PAGE_SYS_NO_AUTH;
+		} catch (ServiceException | ControllerException e) {
+			viewName = PAGE_SYS_SEARCH_NO_DATA;
+		} catch (Exception e) {
+			e.printStackTrace();
+			this.setPageMessage(request, e.getMessage().toString());
+		}
+		mv.setViewName(viewName);
+		return mv;		
+	}
+	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG002D0001Q")
+	@RequestMapping(value = "/core.roleQueryGridJson.do", produces = "application/json")	
+	public @ResponseBody QueryControllerJsonResultObj<List<RoleVO>> queryGrid(SearchValue searchValue, PageOf pageOf) {
+		QueryControllerJsonResultObj<List<RoleVO>> result = this.getQueryJsonResult("CORE_PROG002D0001Q");
+		if (!this.isAuthorizeAndLoginFromControllerJsonResult(result)) {
+			return result;
+		}
+		try {
+			QueryResult<List<RoleVO>> queryResult = this.roleService.findGridResult(searchValue, pageOf);
+			this.setQueryGridJsonResult(result, queryResult, pageOf);
+		} catch (AuthorityException | ServiceException | ControllerException e) {
+			result.setMessage( e.getMessage().toString() );			
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.setMessage( e.getMessage().toString() );
+		}
+		return result;
+	}	
+	
+}
