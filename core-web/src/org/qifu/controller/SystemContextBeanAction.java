c65ddfd6520c6aa593a4544519aeded80ea6d707
Chen Xin Nien <chen.xin.nien@gmail.com>
2017-05-22 16:05:39 +0800

    update

--- core-web/src/org/qifu/controller/SystemContextBeanAction.java
+++ core-web/src/org/qifu/controller/SystemContextBeanAction.java
@@ -31,21 +31,27 @@ import org.qifu.base.exception.AuthorityException;
 import org.qifu.base.exception.ControllerException;
 import org.qifu.base.exception.ServiceException;
 import org.qifu.base.model.ControllerMethodAuthority;
+import org.qifu.base.model.DefaultControllerJsonResultObj;
+import org.qifu.base.model.DefaultResult;
 import org.qifu.base.model.PageOf;
 import org.qifu.base.model.QueryControllerJsonResultObj;
 import org.qifu.base.model.QueryResult;
 import org.qifu.base.model.SearchValue;
+import org.qifu.base.model.YesNo;
+import org.qifu.model.CtxBeanTypes;
 import org.qifu.po.TbSys;
 import org.qifu.po.TbSysCtxBean;
 import org.qifu.service.ISysCtxBeanService;
 import org.qifu.service.ISysService;
 import org.qifu.service.logic.ISystemContextBeanLogicService;
+import org.qifu.util.SimpleUtils;
 import org.qifu.vo.SysCtxBeanVO;
 import org.qifu.vo.SysVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -93,10 +99,27 @@ public class SystemContextBeanAction extends BaseController {
 	
 	private void init(String type, HttpServletRequest request, ModelAndView mv) throws ServiceException, ControllerException, Exception {
 		mv.addObject("sysMap", this.sysService.findSysMap(this.getBasePath(request), true));
+		if ("createPage".equals(type) || "editPage".equals(type)) {
+			mv.addObject("ctxBeanTypesMap", CtxBeanTypes.getTypes(true));
+		}
 	}
 	
 	private void fetchData(SysCtxBeanVO sysCtxBean, ModelAndView mv) throws ServiceException, ControllerException, Exception {
+		DefaultResult<SysCtxBeanVO> result = this.sysCtxBeanService.findObjectByOid(sysCtxBean);
+		if ( result.getValue() == null ) {
+			throw new ControllerException( result.getSystemMessage().getValue() );
+		}
+		sysCtxBean = result.getValue();
+		mv.addObject("sysCtxBean", sysCtxBean);
 		
+		TbSys sys = new TbSys();
+		sys.setSysId(sysCtxBean.getSystem());
+		DefaultResult<TbSys> sResult = this.sysService.findEntityByUK(sys);
+		if ( sResult.getValue() == null ) {
+			throw new ControllerException( sResult.getSystemMessage().getValue() );
+		}
+		sys = sResult.getValue();
+		mv.addObject("systemOid", sys.getOid());
 	}
 	
 	@ControllerMethodAuthority(check = true, programId = "CORE_PROG001D0006Q")
@@ -137,5 +160,145 @@ public class SystemContextBeanAction extends BaseController {
 		}
 		return result;
 	}
-
+	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG001D0006A")
+	@RequestMapping(value = "/core.sysCtxbeanCreate.do")
+	public ModelAndView createPage(HttpServletRequest request) {
+		String viewName = PAGE_SYS_ERROR;
+		ModelAndView mv = this.getDefaultModelAndView("CORE_PROG001D0006A");
+		try {
+			this.init("createPage", request, mv);
+			viewName = "sys-ctxbean/sys-ctxbean-create";
+		} catch (AuthorityException e) {
+			viewName = PAGE_SYS_NO_AUTH;
+		} catch (ServiceException | ControllerException e) {
+			viewName = PAGE_SYS_SEARCH_NO_DATA;
+		} catch (Exception e) {
+			e.printStackTrace();
+			this.setPageMessage(request, e.getMessage().toString());
+		}
+		mv.setViewName(viewName);
+		return mv;
+	}	
+	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG001D0006E")
+	@RequestMapping(value = "/core.sysCtxbeanEdit.do")
+	public ModelAndView editPage(HttpServletRequest request, SysCtxBeanVO sysCtxbean) {
+		String viewName = PAGE_SYS_ERROR;
+		ModelAndView mv = this.getDefaultModelAndView("CORE_PROG001D0006E");
+		try {
+			this.init("editPage", request, mv);
+			this.fetchData(sysCtxbean, mv);
+			viewName = "sys-ctxbean/sys-ctxbean-edit";
+		} catch (AuthorityException e) {
+			viewName = PAGE_SYS_NO_AUTH;
+		} catch (ServiceException | ControllerException e) {
+			viewName = PAGE_SYS_SEARCH_NO_DATA;
+		} catch (Exception e) {
+			e.printStackTrace();
+			this.setPageMessage(request, e.getMessage().toString());
+		}
+		mv.setViewName(viewName);
+		return mv;
+	}		
+	
+	private void checkFields(DefaultControllerJsonResultObj<SysCtxBeanVO> result, SysCtxBeanVO sysCtxbean, String systemOid) throws ControllerException, Exception {
+		this.getCheckControllerFieldHandler(result)
+		.testField("systemOid", ( this.noSelect(systemOid) ), "Please select system!")
+		.testField("className", sysCtxbean, "@org.apache.commons.lang3.StringUtils@isBlank(className)", "Class name is blank!")
+		.testField("type", ( this.noSelect(sysCtxbean.getType()) ), "Please select type!")
+		.throwMessage();
+		boolean rejectClassName = false;
+		String tmp[] = sysCtxbean.getClassName().split("[.]");
+		for (int i=0; tmp!=null && i<tmp.length; i++) {
+			if (!SimpleUtils.checkBeTrueOf_azAZ09(tmp[i])) {
+				rejectClassName = true;
+			}
+		}
+		this.getCheckControllerFieldHandler(result).testField("className", rejectClassName, "Class name not accept!").throwMessage();
+	}
+	
+	private void save(DefaultControllerJsonResultObj<SysCtxBeanVO> result, SysCtxBeanVO sysCtxbean, String systemOid) throws AuthorityException, ControllerException, ServiceException, Exception {
+		this.checkFields(result, sysCtxbean, systemOid);
+		DefaultResult<SysCtxBeanVO> cResult = this.systemContextBeanLogicService.create(sysCtxbean, systemOid);
+		if ( cResult.getValue() != null ) {
+			result.setValue( cResult.getValue() );
+			result.setSuccess( YesNo.YES );
+		}
+		result.setMessage( cResult.getSystemMessage().getValue() );
+	}
+	
+	private void update(DefaultControllerJsonResultObj<SysCtxBeanVO> result, SysCtxBeanVO sysCtxbean, String systemOid) throws AuthorityException, ControllerException, ServiceException, Exception {
+		this.checkFields(result, sysCtxbean, systemOid);
+		DefaultResult<SysCtxBeanVO> uResult = this.systemContextBeanLogicService.update(sysCtxbean, systemOid);
+		if ( uResult.getValue() != null ) {
+			result.setValue( uResult.getValue() );
+			result.setSuccess( YesNo.YES );
+		}
+		result.setMessage( uResult.getSystemMessage().getValue() );
+	}	
+	
+	private void delete(DefaultControllerJsonResultObj<Boolean> result, SysCtxBeanVO sysCtxbean) throws AuthorityException, ControllerException, ServiceException, Exception {
+		DefaultResult<Boolean> dResult = this.systemContextBeanLogicService.delete(sysCtxbean);
+		if ( dResult.getValue() != null && dResult.getValue() ) {
+			result.setValue( dResult.getValue() );
+			result.setSuccess( YesNo.YES );
+		}
+		result.setMessage( dResult.getSystemMessage().getValue() );		
+	}
+	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG001D0006A")
+	@RequestMapping(value = "/core.sysCtxbeanSaveJson.do", produces = "application/json")		
+	public @ResponseBody DefaultControllerJsonResultObj<SysCtxBeanVO> doSave(SysCtxBeanVO sysCtxbean, @RequestParam("systemOid") String systemOid) {
+		DefaultControllerJsonResultObj<SysCtxBeanVO> result = this.getDefaultJsonResult("CORE_PROG001D0006A");
+		if (!this.isAuthorizeAndLoginFromControllerJsonResult(result)) {
+			return result;
+		}
+		try {
+			this.save(result, sysCtxbean, systemOid);
+		} catch (AuthorityException | ServiceException | ControllerException e) {
+			result.setMessage( e.getMessage().toString() );			
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.setMessage( e.getMessage().toString() );
+		}
+		return result;
+	}
+	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG001D0006E")
+	@RequestMapping(value = "/core.sysCtxbeanUpdateJson.do", produces = "application/json")		
+	public @ResponseBody DefaultControllerJsonResultObj<SysCtxBeanVO> doUpdate(SysCtxBeanVO sysCtxbean, @RequestParam("systemOid") String systemOid) {
+		DefaultControllerJsonResultObj<SysCtxBeanVO> result = this.getDefaultJsonResult("CORE_PROG001D0006E");
+		if (!this.isAuthorizeAndLoginFromControllerJsonResult(result)) {
+			return result;
+		}
+		try {
+			this.update(result, sysCtxbean, systemOid);
+		} catch (AuthorityException | ServiceException | ControllerException e) {
+			result.setMessage( e.getMessage().toString() );			
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.setMessage( e.getMessage().toString() );
+		}
+		return result;
+	}
+	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG001D0006D")
+	@RequestMapping(value = "/core.sysCtxbeanDeleteJson.do", produces = "application/json")		
+	public @ResponseBody DefaultControllerJsonResultObj<Boolean> doUpdate(SysCtxBeanVO sysCtxbean) {
+		DefaultControllerJsonResultObj<Boolean> result = this.getDefaultJsonResult("CORE_PROG001D0006D");
+		if (!this.isAuthorizeAndLoginFromControllerJsonResult(result)) {
+			return result;
+		}
+		try {
+			this.delete(result, sysCtxbean);
+		} catch (AuthorityException | ServiceException | ControllerException e) {
+			result.setMessage( e.getMessage().toString() );			
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.setMessage( e.getMessage().toString() );
+		}
+		return result;
+	}	
+	
 }
