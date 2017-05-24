01b94d36734dbe7d712680bb5232d69f669a60d3
Chen Xin Nien <chen.xin.nien@gmail.com>
2017-05-24 13:56:47 +0800

    update

--- core-web/src/org/qifu/controller/SystemExpressionAction.java
+++ core-web/src/org/qifu/controller/SystemExpressionAction.java
@@ -21,11 +21,21 @@
  */
 package org.qifu.controller;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.qifu.base.controller.BaseController;
+import org.qifu.base.exception.AuthorityException;
 import org.qifu.base.exception.ControllerException;
 import org.qifu.base.exception.ServiceException;
+import org.qifu.base.model.ControllerMethodAuthority;
+import org.qifu.base.model.PageOf;
+import org.qifu.base.model.QueryControllerJsonResultObj;
+import org.qifu.base.model.QueryResult;
+import org.qifu.base.model.ScriptTypeCode;
+import org.qifu.base.model.SearchValue;
 import org.qifu.po.TbSysExpression;
 import org.qifu.service.ISysExpressionService;
 import org.qifu.service.logic.ISystemExpressionLogicService;
@@ -33,6 +43,8 @@ import org.qifu.vo.SysExpressionVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -65,10 +77,51 @@ public class SystemExpressionAction extends BaseController {
 		this.systemExpressionLogicService = systemExpressionLogicService;
 	}
 	
+	private void init(String type, HttpServletRequest request, ModelAndView mv) throws ServiceException, ControllerException, Exception {
+		mv.addObject("typeMap", ScriptTypeCode.getTypeMap(true));
+	}
+	
 	private void fetchData(SysExpressionVO sysExpression, ModelAndView mv) throws ServiceException, ControllerException, Exception {
 		
 	}
 	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG003D0002Q")
+	@RequestMapping(value = "/core.sysExpressionManagement.do")	
+	public ModelAndView queryPage(HttpServletRequest request) {
+		String viewName = PAGE_SYS_ERROR;
+		ModelAndView mv = this.getDefaultModelAndView("CORE_PROG003D0002Q");
+		try {
+			this.init("queryPage", request, mv);
+			viewName = "sys-expression/sys-expression-management";
+		} catch (AuthorityException e) {
+			viewName = PAGE_SYS_NO_AUTH;
+		} catch (ServiceException | ControllerException e) {
+			viewName = PAGE_SYS_SEARCH_NO_DATA;
+		} catch (Exception e) {
+			e.printStackTrace();
+			this.setPageMessage(request, e.getMessage().toString());
+		}
+		mv.setViewName(viewName);
+		return mv;
+	}		
 	
+	@ControllerMethodAuthority(check = true, programId = "CORE_PROG003D0002Q")
+	@RequestMapping(value = "/core.sysExpressionQueryGridJson.do", produces = "application/json")	
+	public @ResponseBody QueryControllerJsonResultObj< List<SysExpressionVO> > queryGrid(SearchValue searchValue, PageOf pageOf) {
+		QueryControllerJsonResultObj< List<SysExpressionVO> > result = this.getQueryJsonResult("CORE_PROG003D0002Q");
+		if (!this.isAuthorizeAndLoginFromControllerJsonResult(result)) {
+			return result;
+		}
+		try {
+			QueryResult< List<SysExpressionVO> > queryResult = this.sysExpressionService.findGridResult(searchValue, pageOf);
+			this.setQueryGridJsonResult(result, queryResult, pageOf);
+		} catch (AuthorityException | ServiceException | ControllerException e) {
+			result.setMessage( e.getMessage().toString() );
+		} catch (Exception e) {
+			e.printStackTrace();
+			result.setMessage( e.getMessage().toString() );
+		}
+		return result;
+	}
 	
 }
