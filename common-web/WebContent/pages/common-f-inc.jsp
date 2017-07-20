6bd506523433e2d72c3b442daf9a0bf7a6c3b683
Chen Xin Nien <chen.xin.nien@gmail.com>
2017-07-20 09:24:13 +0800

    Update add common-web need resource files.

---
+++ common-web/WebContent/pages/common-f-inc.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@page import="org.qifu.base.Constants"%>
+<%@page import="org.qifu.util.ApplicationSiteUtils"%>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+
+String mainBasePath = basePath;
+if (!Constants.getMainSystem().equals( Constants.getSystem() ) ) {
+	mainBasePath = ApplicationSiteUtils.getBasePath(Constants.getMainSystem(), request);
+}
+
+%>
+<link rel="stylesheet" href="<%=mainBasePath%>/tether/tether.min.css?ver=${jsVerBuild}" crossorigin="anonymous">
+<script type="text/javascript" src="<%=mainBasePath%>/tether/tether.min.js?ver=${jsVerBuild}"></script>
+<script type="text/javascript" src="<%=mainBasePath%>/jquery/jquery-3.1.1.min.js?ver=${jsVerBuild}"></script>
+<link rel="stylesheet" href="<%=mainBasePath%>/bootstrap-4.0.0-alpha.6/css/bootstrap.css?ver=${jsVerBuild}" crossorigin="anonymous">
+<link href="<%=mainBasePath%>/font-awesome/css/font-awesome.min.css?ver=${jsVerBuild}" rel="stylesheet" type="text/css" />
+<script src="<%=mainBasePath%>/bootstrap-4.0.0-alpha.6/js/bootstrap.js?ver=${jsVerBuild}" crossorigin="anonymous"></script>
+<script src="<%=mainBasePath%>/configJs.do?ver=${jsVerBuild}" crossorigin="anonymous"></script>
+<script src="<%=mainBasePath%>/js/f.js?ver=${jsVerBuild}" crossorigin="anonymous"></script>
--- common-web/src/org/qifu/common/controller/TestAction.java
+++ common-web/src/org/qifu/common/controller/TestAction.java
@@ -1,5 +1,59 @@
+/* 
+ * Copyright 2012-2017 qifu of copyright Chen Xin Nien
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * -----------------------------------------------------------------------
+ * 
+ * author: 	Chen Xin Nien
+ * contact: chen.xin.nien@gmail.com
+ * 
+ */
 package org.qifu.common.controller;
 
-public class TestAction {
+import javax.servlet.http.HttpServletRequest;
+
+import org.qifu.base.controller.BaseController;
+import org.qifu.base.exception.AuthorityException;
+import org.qifu.base.exception.ControllerException;
+import org.qifu.base.exception.ServiceException;
+import org.qifu.base.model.ControllerMethodAuthority;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+public class TestAction extends BaseController {
+	
+	private void init(String type, HttpServletRequest request, ModelAndView mv) throws ServiceException, ControllerException, Exception {
+		
+	}
+	
+	@ControllerMethodAuthority(check = true, programId = "COMMON_PROG001D0001Q")
+	@RequestMapping(value = "/common.test.do")	
+	public ModelAndView queryPage(HttpServletRequest request) {
+		String viewName = PAGE_SYS_ERROR;
+		ModelAndView mv = this.getDefaultModelAndView("COMMON_PROG001D0001Q");
+		try {
+			this.init("queryPage", request, mv);
+			viewName = "test/hello";
+		} catch (AuthorityException e) {
+			viewName = this.getAuthorityExceptionPage(e, request);
+		} catch (ServiceException | ControllerException e) {
+			viewName = this.getServiceOrControllerExceptionPage(e, request);
+		} catch (Exception e) {
+			this.getExceptionPage(e, request);
+		}
+		mv.setViewName(viewName);
+		return mv;		
+	}	
 
 }
